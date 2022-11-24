c0ba5a2b6b92dedbab7283e02c03bde065fdc83a
drozboril <rozborildavid77@gmail.com>
2022-11-24 20:44:08 +0100

    Uživatel zadá hodnotu a exponent, program vypíše výsledek mocnění, které probíhá v samostatné metodě

---
+++ Mocnina metoda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mocnina_metoda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mocnina čísla\nZadejte číslo");
+            double cislo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Zadejte mocninu");
+            double mocnina = double.Parse(Console.ReadLine());
+            Console.WriteLine("Výsledek umocnění je {0}", Mocnina(cislo, mocnina));
+            Console.ReadLine();
+        }
+        static double Mocnina(double cislo, double mocnina)
+        {
+            double vypocet = Math.Pow(cislo, mocnina);
+            return vypocet;
+        }
+    }
+}
